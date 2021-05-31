2e5772108319d7aeb07939250e30221e364aff15
admin <admin@YD03-19>
2021-05-31 18:26:43 +0900

    2021.05.31

--- 4일차/src/co/yedam/member/MemberService.java
+++ 4일차/src/co/yedam/member/MemberService.java
@@ -3,43 +3,52 @@ package co.yedam.member;
 import java.util.Scanner;
 
 public class MemberService {
-	Member[] members=new Member[10];
-	int cnt =0;
+	Member[] members = new Member[10];
+	int cnt = 0;
 	Scanner scanner = new Scanner(System.in);
-	
-	//회원가입
+
+	// 회원가입
 //	void insert(String id, String password, String name, int age) {
 //		members[cnt]= new Member(id, password, name, age);
 //		cnt++;
 //	}
 	void insert() {
+		System.out.print("id>");
 		String id = scanner.next();
-		String password =scanner.next();
-		String name= scanner.next();
-		int age= scanner.nextInt();
-		members[cnt]= new Member(id, password, name, age);
+		System.out.print("password>");
+		String password = scanner.next();
+		System.out.print("name>");
+		String name = scanner.next();
+		System.out.print("age>");
+		int age = scanner.nextInt();
+		members[cnt] = new Member(name, id, password, age);
 		cnt++;
 	}
-	//회원리스트
+
+	// 회원리스트
 	void printList() {
-		for(int i=0;i<cnt;i++) {
+		for (int i = 0; i < cnt; i++) {
 			System.out.println(members[i]);
 		}
 	}
-	
+
 	void printMember(int i) {
 		System.out.println(members[i].id);
 		System.out.println(members[i].name);
 		System.out.println(members[i].age);
 	}
-	
+
 	boolean login(String id, String password) {
 		boolean result = false;
-		for(int i=0; i<cnt; i++) {
-			if (id.equals(members[i].id) & password.equals(members[i].password)){
-			result = true;
-			System.out.println("로그인되었습니다.");
-			break;}
+		for (int i = 0; i < cnt; i++) {
+			if (id.equals(members[i].id) && password.equals(members[i].password)) {
+				result = true;
+				System.out.println("로그인되었습니다.");
+				break;
+			}
+		}
+		if (result==false) {
+			System.out.println("id 또는 패스워드 틀림");
 		}
 		return result;
 //		if (id.equals("hong") & password.equals("12345")){
@@ -47,6 +56,7 @@ public class MemberService {
 //		else {
 //		return false;}
 	}
+
 	void logout(String id) {
 		System.out.println("로그아웃 되었습니다.");
 	}
